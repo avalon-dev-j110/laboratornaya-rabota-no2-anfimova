132b497832973b67353d97dbb202ec5d754cbe8f
anastasiia.anfimova <anastasiia.anfimova@gmail.com>
2020-11-04 23:41:05 +0300

    all

--- src/ru/avalon/java/dev/j10/labs/shapes/Circle.java
+++ src/ru/avalon/java/dev/j10/labs/shapes/Circle.java
@@ -1,5 +1,7 @@
 package ru.avalon.java.dev.j10.labs.shapes;
 
+import java.util.Random;
+
 /**
  * Представление об окружности.
  * <p>
@@ -8,11 +10,37 @@ package ru.avalon.java.dev.j10.labs.shapes;
  *
  * @see <a href="https://ru.wikipedia.org/wiki/%D0%9E%D0%BA%D1%80%D1%83%D0%B6%D0%BD%D0%BE%D1%81%D1%82%D1%8C">Окружность</a>
  */
-public class Circle {
+
+public class Circle implements Point {
 
     /*
      * TODO: Реализовать класс 'Circle'
      * 1. Используйте наследование.
      * 2. Реализуйте все абстрактные методы.
      */
+
+    private float x;
+    private float y;
+
+    public Circle() {
+        this.x = getX();
+        this.y = getY();
+    }
+
+    public float getX() {
+        Random random = new Random();
+        float x = random.nextInt(5) + 1;
+        return x;
+    }
+
+    public float getY() {
+        Random random = new Random();
+        float y = random.nextInt(5) + 1;
+        return y;
+    }
+
+    public float getArea() {
+        float radius = (float) Math.sqrt(x*x + y*y);
+        return (float) (2*radius*Math.PI);
+    }
 }
--- src/ru/avalon/java/dev/j10/labs/shapes/Polygon.java
+++ src/ru/avalon/java/dev/j10/labs/shapes/Polygon.java
@@ -9,7 +9,8 @@ package ru.avalon.java.dev.j10.labs.shapes;
  *
  * @see <a href="https://ru.wikipedia.org/wiki/%D0%9C%D0%BD%D0%BE%D0%B3%D0%BE%D1%83%D0%B3%D0%BE%D0%BB%D1%8C%D0%BD%D0%B8%D0%BA">Многоугольник</a>
  */
-public interface Polygon {
+
+public interface Polygon extends Shape{
 
     /*
      * TODO: Закончить определение интерфейса 'Polygon'
@@ -18,7 +19,7 @@ public interface Polygon {
      *
      * 2. При необходимости, дополните структуру интерфейса.
      */
-
+    float getSide();
 
     /**
      * Возвращает периметр многоугольника.
